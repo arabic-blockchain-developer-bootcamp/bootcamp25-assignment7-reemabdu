ec6db74b5f46e1726ec188093d4d0e3f4796730f
Reem Abdulglil Hassan Saleh <reemabdulglilhassansaleh@Reems-MacBook-Air.local>
2025-03-17 12:46:10 +0300

    Assignment7

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -3,16 +3,21 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract Assignment7 is ERC20 {
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
-    constructor() ERC20() {}
+    constructor(string memory _name,string memory _symbel) ERC20(_name,_symbel){
+
+    }
 
     // Function to mint tokens
     // Fill in the visibility modifier to be external
     // Set params as to address and amount
-    function mint() {
+   function mint(address to, uint256 amount)  public {
         // Fill in the logic
         // call _mint function with to and amount params
+        _mint(to,amount);
     }
 }
